a31d22999d47f7fd9c43f2cc7e31ea1b249e9df0
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2024-12-17 00:51:17 -0600

    add part 2 of day 1

--- 2024/Day1/Puzzle1.py
+++ 2024/Day1/Puzzle1.py
@@ -1,6 +1,14 @@
 first = []
 second = []
 
+def get_count(arr,target):
+    count = 0
+    for i in arr:
+        if(i==target):
+            count+=1
+    return count
+
+
 def sort_list(val):
     for i in range(0,len(val)):
         min = i
@@ -20,13 +28,24 @@ with open('Puzzle_1_input.txt','r') as file:
 
 sorted_first = sort_list(first)
 sorted_second = sort_list(second)
-# print(second)
-total = 0
+total_difference = 0
 for idx,x in enumerate(sorted_first):
     y  = sorted_second[idx]
     diff = abs(x - y)
     # print(diff,x,y)
-    total += diff
+    total_difference += diff
 
+total_similarity = 0
+for idx,x  in enumerate(sorted_first):
+    count = get_count(sorted_second,x);
+    similarity = x * count
+    total_similarity += similarity
 
-print(total)
+# only unique elements from list a
+# but it is not necessary here
+# seen = set()
+# unique_first = [val for val in sorted_first if val not in seen and (seen.add(val)) or True]
+
+print(total_similarity)
+
+print(total_difference)
