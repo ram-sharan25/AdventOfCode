002cab5aa90ee8d324d3c8fa842f0541ca81c84c
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2024-12-17 20:20:44 -0600

    add part two of day 3

--- 2024/Day3/Mull_over.py
+++ 2024/Day3/Mull_over.py
@@ -11,13 +11,35 @@ def get_all_expressions():
     return all
 
 
-arr   = get_all_expressions()
-total = 0
-for nums in arr:
-    print()
-    for num in nums:
-       print(num)
-       total+= num[0]*num[1]
+def get_all_expressions_with_do_dont():
+    all = []
+    with open('input.txt','r') as f:
+        for line in f :
+            val     = re.findall(r"(mul\(\d+,\d+\)|don't\(\)|do\(\))", line)
+            all.append(val)
+    return all
 
+def part_one():
+    arr   = get_all_expressions()
+    total = 0
+    for nums in arr:
+        for num in nums:
+            total+= num[0]*num[1]
+    print("Part one: ",total)
 
-print(total)
+def part_two():
+    all = get_all_expressions_with_do_dont()
+    total=0
+    mul = True
+    for collection in all:
+        for  val in collection:
+            if(val=="don't()"):
+                mul = False
+            elif(val=="do()"):
+                mul = True
+            elif mul:
+                numbers = re.findall(r'\d{1,3}', val)
+                total+= int(numbers[0])*int(numbers[1])
+    print("Part two: ",total)
+part_one()
+part_two()
